7e09d0f9a1b517d4f15b1a99e3a7153e29cbef28
RFENZL <92161747+RFENZL@users.noreply.github.com>
2026-03-24 15:22:39 +0100

    image priority

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default async function HomePage() {
               height={500}
               className={styles.heroImage}
               unoptimized
+              priority
             />
           ) : (
             <div className={styles.heroPlaceholder} />
